7f067c0fcbfac992109b565906668cad811dac39
christopherbaa <chbaasner@gmail.com>
2022-06-09 15:11:34 +0200

    Initial Commit

---
+++ src/main/java/de/nutposit/javabackenddeveloper_recipes/JavaBackendDeveloperRecipesApplication.java
@@ -0,0 +1,13 @@
+package de.nutposit.javabackenddeveloper_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendDeveloperRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBackendDeveloperRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/nutposit/javabackenddeveloper_recipes/JavaBackendDeveloperRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nutposit.javabackenddeveloper_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendDeveloperRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
