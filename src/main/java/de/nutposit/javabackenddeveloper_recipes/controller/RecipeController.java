f51c5e9d04c7f11a45cb0162bb9c704bc60227b6
christopherbaa <chbaasner@gmail.com>
2022-06-09 15:31:18 +0200

    Stage 1 Submission - Added RecipeController and implemented the Endpoints

---
+++ src/main/java/de/nutposit/javabackenddeveloper_recipes/controller/RecipeController.java
@@ -0,0 +1,29 @@
+package de.nutposit.javabackenddeveloper_recipes.controller;
+
+import de.nutposit.javabackenddeveloper_recipes.model.Recipe;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/recipe")
+public class RecipeController {
+
+    private Recipe recipe;
+
+    public RecipeController() {
+        this.recipe = new Recipe();
+    }
+
+    @GetMapping
+    public Recipe getRecipes() {
+        return this.recipe;
+    }
+
+    @PostMapping
+    public void postRecipe(@RequestBody Recipe recipe) {
+        setRecipe(recipe);
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
