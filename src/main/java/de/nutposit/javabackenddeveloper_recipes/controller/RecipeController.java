71ec1b8a05af4dc77c4073d2dd07ab8afa208a2d
christopherbaa <chbaasner@gmail.com>
2022-06-10 17:07:00 +0200

    Stage 2 Submission - Modified Recipe accordingly and added the new Endpoints

--- src/main/java/de/nutposit/javabackenddeveloper_recipes/controller/RecipeController.java
+++ src/main/java/de/nutposit/javabackenddeveloper_recipes/controller/RecipeController.java
@@ -1,26 +1,40 @@
 package de.nutposit.javabackenddeveloper_recipes.controller;
 
+import de.nutposit.javabackenddeveloper_recipes.model.RecipeIdentifier;
 import de.nutposit.javabackenddeveloper_recipes.model.Recipe;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 
 @RestController
 @RequestMapping("/api/recipe")
 public class RecipeController {
 
-    private Recipe recipe;
+    private ArrayList<Recipe> recipes;
 
     public RecipeController() {
-        this.recipe = new Recipe();
+        this.recipes = new ArrayList<>();
     }
 
     @GetMapping
-    public Recipe getRecipes() {
-        return this.recipe;
+    public ArrayList<Recipe> getRecipes() {
+        return this.recipes;
     }
 
-    @PostMapping
-    public void postRecipe(@RequestBody Recipe recipe) {
-        this.recipe = recipe;
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable("id") int id) {
+        return id < this.recipes.size() && id >= 0 ?
+                new ResponseEntity<>(this.recipes.get(id), HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<RecipeIdentifier> postRecipe(@RequestBody Recipe recipe) {
+        this.recipes.add(recipe);
+        return new ResponseEntity<>(new RecipeIdentifier(this.recipes.indexOf(recipe)), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/de/nutposit/javabackenddeveloper_recipes/model/RecipeIdentifier.java
@@ -0,0 +1,21 @@
+package de.nutposit.javabackenddeveloper_recipes.model;
+
+public class RecipeIdentifier {
+
+    int id;
+
+    public RecipeIdentifier(int id) {
+        this.id = id;
+    }
+
+    public RecipeIdentifier() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
